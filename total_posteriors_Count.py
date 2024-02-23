a8efbe010464708ef12f508d53145bdfe9586ee1
Paula Ramos-Silva <paula.rsilva@naturalis.nl>
2024-02-23 13:25:24 +0100

    Update total_posteriors_Count.py
    Edits according to PEP8

--- total_posteriors_Count.py
+++ total_posteriors_Count.py
@@ -1,17 +1,36 @@
-__author__ = 'Paula Ramos-Silva'
-# ======================================================================
-# Print total presences, multi, gains, losses, expansions, contractions from Posteriors - Count
-# Count software for analysis of gene content evolution is
-# available at http://www.iro.umontreal.ca/~csuros/gene_content/count.html
-#
-# ======================================================================
-#
-# Tested with:
-#  Python 2.7.1
-#  Python 3.9.0
-#
-# Created 25-11-2016
-# Updated 08-02-2024
+__author__ = "Paula Ramos-Silva"
+__dateCreated__ = "25-11-2016"
+__dateModified__ = "23-02-2024"
+
+"""
+Given a phylogenetic profile as input, COUNT computes posterior probabilities for every gene, including:
+- gains, losses, expansions and contractions at tree edges
+- presences and multiple members at tree nodes
+
+This data is retrieved in a large table and processed here with total_posteriors_Count.py to estimate the total number
+of gains, losses, presences, expansions, contractions and multiple presences in a readable format. 
+
+
+usage: python total_posteriors_Count.py --posteriors [POSTERIORS FILE]
+
+References: 
+1. Count - software for analysis of gene content evolution is available at 
+http://www.iro.umontreal.ca/~csuros/gene_content/count.html
+
+2. Miklós Csűös, Count: evolutionary analysis of phylogenetic profiles with parsimony and likelihood, Bioinformatics, 
+Volume 26, Issue 15, August 2010, Pages 1910–1912, https://doi.org/10.1093/bioinformatics/btq315
+
+3. Paula Ramos-Silva, Mónica Serrano, Adriano O Henriques, From Root to Tips: Sporulation Evolution and Specialization 
+in Bacillus subtilis and the Intestinal Pathogen Clostridioides difficile, Molecular Biology and Evolution, Volume 36, 
+Issue 12, December 2019, Pages 2714–2736, https://doi.org/10.1093/molbev/msz175
+
+Usage:
+
+Tested with:
+Python 2.7.1
+Python 3.9.0
+
+"""
 
 import argparse
 import pandas
@@ -19,9 +38,12 @@ import re
 import math
 import sys
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Print total presences, multi, gains, losses, expansions, contractions from Posteriors - Count")
-    parser.add_argument('--posteriors', dest='posteriors', help='csv file with posteriors file with comment lines removed')
+    parser = argparse.ArgumentParser(description="Print total presences, multi, gains, losses, expansions, "
+                                                 "contractions from Posteriors - Count")
+    parser.add_argument('--posteriors', dest='posteriors',
+                        help='csv file with posteriors file with comment lines removed')
 
     args = parser.parse_args()
 
@@ -30,38 +52,35 @@ def main():
     column_names = list(posteriors_table.columns.values)
     row_names = list(posteriors_table.index)
 
-    column_types = []
-    column_types.append(filter(lambda x:re.search(r':present', x), column_names))
-    column_types.append(filter(lambda x:re.search(r':multi', x), column_names))
-    column_types.append(filter(lambda x:re.search(r':gain', x), column_names))
-    column_types.append(filter(lambda x:re.search(r':loss', x), column_names))
-    column_types.append(filter(lambda x:re.search(r':expansion', x), column_names))
-    column_types.append(filter(lambda x:re.search(r':contraction', x), column_names))
+    column_types = [filter(lambda x: re.search(r':present', x), column_names),
+                    filter(lambda x: re.search(r':multi', x), column_names),
+                    filter(lambda x: re.search(r':gain', x), column_names),
+                    filter(lambda x: re.search(r':loss', x), column_names),
+                    filter(lambda x: re.search(r':expansion', x), column_names),
+                    filter(lambda x: re.search(r':contraction', x), column_names)]
 
     for ct in column_types:
-        countTotalbyNode(row_names, ct, posteriors_table)
+        count_total_by_node(row_names, ct, posteriors_table)
 
 
-def countTotalbyNode(rows, columns, posteriorsdf):
-
+def count_total_by_node(rows, columns, posteriors):
     for node in columns:
 
         total = 0
 
         for index in rows:
 
-            if not math.isnan(posteriorsdf.at[index, node]):
+            if not math.isnan(posteriors.at[index, node]):
+                total += float(posteriors.at[index, node])
 
-                total += float(posteriorsdf.at[index, node])
-
-        print ('{}\t{}'.format(node, int(round(total))))
+        print('{}\t{}'.format(node, int(round(total))))
 
 
 #########################
 
-if __name__=="__main__":
-    usage = "python total_posteriors_Count.py --posteriors <your_posteriors_table.csv>"
-    if len(sys.argv) != 1:
-        print("Incorrect arguments.\nUsage is : ", usage)
+if __name__ == "__main__":
+    usage = "python total_posteriors_Count.py -h"
+    if len(sys.argv) == 1:
+        print("Incorrect arguments.\nFor usage try: ", usage)
         sys.exit()
     main()
